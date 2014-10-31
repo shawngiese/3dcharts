6c779fe9b7cfc7a500ab4acdb49c9ae2a9f9e265
shawngiese <shawngiese@gmail.com>
2014-10-31 10:47:49 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -5,22 +5,19 @@ d3.xhr(url)
     .header("X-Requested-With", "XMLHttpRequest")
     .header("Content-Type", "application/x-www-form-urlencoded")
     .post("delay=3", function (error, request) {
-        //data = request.response;
         data = jQuery.parseJSON(request.response);
-        console.log(data);
-    if (error) return console.warn(error.responseText);
+        if (error) return console.warn(error.responseText);
 
 //d3.xhr loads JSON from GitHub, to load JSON from a REST API endpoint
 //       comment out d3.xhr and enable the following line d3.json...
 //d3.json(url, function (error, data) {
 
-      // Aggregate the JSON data for use chart building in viz.js
-
+      // Aggregate the JSON data for chart building in viz.js
          var nestedData = d3.nest()
          .key(function(d) { return d.customer; })
           .entries(data.data);
 
-          //recreate field names after change made by nesting
+          //recreate field names after changes made by nesting
          nestedData.forEach(function(d) {
           d.customer = d.key;
           d.recs = d.values;
@@ -32,8 +29,6 @@ d3.xhr(url)
         if (v.recs[1] != null) {delete v.recs[1].customer };
         if (v.recs[2] != null) {delete v.recs[2].customer }
          });
-
-        //console.log(nestedData);
 
         VIZ.drawElements(nestedData);
         VIZ.transform('helix');
